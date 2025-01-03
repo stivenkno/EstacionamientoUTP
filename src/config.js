2792f3381f1f3c95fc37408ed08f6c174712acdd
stivenkno <stivencano931@gmail.com>
2025-01-03 01:13:40 -0500

    sasd

--- src/config.js
+++ src/config.js
@@ -9,16 +9,26 @@ const DB_URL = VAR_ENTORNO.DB_URL;
 
 //Conexion a la base de datos
 const pool = new Pool({
-  connectionString: DB_URL,
+  connectionString: process.env.DATABASE_URL || DB_URL,
   ssl: {
     rejectUnauthorized: false,
   },
 });
 
-pool.query("SELECT NOW()", (err, res) => {
-  if (err) throw err;
-  console.log(`Connected to the database at ${res.rows[0].now}`);
-});
+(async () => {
+  try {
+    const client = await pool.connect();
+    console.log("Conexión exitosa a PostgreSQL");
+
+    // Realizar una consulta de prueba
+    const res = await client.query("SELECT NOW() AS current_time");
+    console.log("Hora actual en la base de datos:", res.rows[0].current_time);
+
+    client.release(); // Liberar el cliente
+  } catch (err) {
+    console.error("Error al conectar a PostgreSQL:", err);
+  }
+})();
 
 const createTables = async () => {
   try {
